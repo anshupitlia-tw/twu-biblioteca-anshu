3ddc3c7644521aff7f391c42705fb952369b2d02
anshup <anshup@INanshup.local>
2015-09-03 19:52:20 +0530

    code for the start to call print in user interface with the welcome message taken from the messages

--- src/com/twu/biblioteca/LibraryApplication.java
+++ src/com/twu/biblioteca/LibraryApplication.java
@@ -1,14 +1,20 @@
 package com.twu.biblioteca;
 
+import java.util.HashMap;
+
 //The Bangalore Public Library
 public class LibraryApplication {
     private UserInterface userInterface;
+    private Messages messages;
 
     public LibraryApplication(UserInterface userInterface) {
         this.userInterface = userInterface;
+        HashMap<String, String> setUpMessages = new HashMap<>();
+        setUpMessages.put("welcome_message", "Welcome! Biblioteca at your service");
+        messages = new Messages(setUpMessages);
     }
 
     public void start() {
-        userInterface.print("Welcome! Biblioteca at your service");
+        userInterface.print(messages.getUXMessage("welcome_message"));
     }
 }
