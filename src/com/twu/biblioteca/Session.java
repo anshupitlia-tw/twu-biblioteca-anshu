72736b5b6d0f98372596cde7057ee8f76ea80c65
anshup <anshup@INanshup.local>
2015-09-15 14:38:31 +0530

    A session should be able to return the currently logged in user

---
+++ src/com/twu/biblioteca/Session.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+public class Session {
+    private User currentUser;
+
+    public Session(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+}
---
+++ test/com/twu/biblioteca/SessionTest.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SessionTest {
+
+    @Test
+    public void shouldBeAbleToGiveTheCurrentlyLoggedInUser() {
+        User currentUser = new User(new UserCredentials("bib-0001", "password"));
+        Session session = new Session(currentUser);
+
+        assertEquals(currentUser, session.getCurrentUser());
+    }
+}
